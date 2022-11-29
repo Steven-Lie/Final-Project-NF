e93f7510e193cdd72c625ec62276ae169d898ada
Steven-Lie <s.lie01.sl@gmail.com>
2022-11-29 11:27:46 +0700

    workspace initial

---
+++ lib/workspace.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Workspace extends StatelessWidget {
+  const Workspace({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
